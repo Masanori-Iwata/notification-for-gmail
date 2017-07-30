7f782bb88793fa3e57b480b531563d987a6cb3a0
iwatamasanori <futibolartistico@gmail.com>
2017-07-31 02:53:20 +0900

    Improvement of reception function.

--- background.js
+++ background.js
@@ -59,229 +59,207 @@ class BadgeAnimate {
 
 }
 
-class MailChecker extends BadgeAnimate {
+// defaults.js
+// const defaults = {...};
+let options = null;
+const badgeAnimate = new BadgeAnimate();
+const badgeData = {
+  path: [ 'icon_gmail_not_logged_in_01.png', 'icon_gmail_logged_in_01.png' ],
+  text: [ '', '' ]
+};
+const xhr = new XMLHttpRequest();
 
-  static init( defaults ) {
+chrome.storage.local.get( defaults, ( opt ) => options = opt );
+chrome.runtime.onInstalled.addListener( onInit );
+addListenerPolling();
+addListenerReceive();
+addListenerBadgeClicked();
 
-    const handler = ( options ) => new MailChecker( defaults, options );
-    return chrome.storage.local.get( defaults, handler );
+function onInit() {
 
-  }
+  polling();
+  chrome.alarms.create( 'polling', {periodInMinutes: getPollIntervalTime()} );
 
-  static badgeData() {
+}
 
-    return {
-      path: [ 'icon_gmail_not_logged_in_01.png', 'icon_gmail_logged_in_01.png' ],
-      text: [ '', '' ]
-    };
+function addListenerPolling() {
 
-  }
+  chrome.alarms.onAlarm.addListener( polling.bind( this ) );
 
-  constructor( defaults, options ) {
+}
 
-    super();
+function addListenerReceive() {
 
-    this.core = this.constructor;
-    this.defaults = Object.assign( {}, defaults );
-    this.options = Object.assign( {}, options );
-    this.badgeData = Object.assign( {}, this.core.badgeData() );
-    this.init();
+  const handler = ( data ) => {
 
-  }
+    badgeData.text[ 1 ] = data.text[ 1 ];
 
-  init() {
+  };
 
-    this.xhr = new XMLHttpRequest();
+  chrome.storage.local.get( badgeData, handler );
 
-    this.onReceive();
-    this.onPolling();
-    this.onBadgeClicked();
+  optionsChanged();
 
-  }
+}
 
-  onReceive() {
+function addListenerBadgeClicked() {
 
-    const handler = ( data ) => {
+  chrome.browserAction.onClicked.addListener( () => {
 
-      this.badgeData.text[ 1 ] = data.text[ 1 ];
+    createTab();
+    polling( true );
 
-    };
+  } );
 
-    chrome.storage.local.get( this.badgeData, handler );
+}
 
-    this.optionsChanged();
+function optionsChanged() {
 
-  }
+  const handler = ( data ) => {
 
-  optionsChanged() {
+    options = Object.assign( {}, data );
 
-    const handler = ( data ) => {
+    chrome.alarms.clear( 'polling' );
+    chrome.alarms.create( 'polling', {periodInMinutes: getPollIntervalTime()} );
+    polling( true );
 
-      this.options = Object.assign( {}, data );
+  };
 
-      chrome.alarms.clear( 'polling' );
-      chrome.alarms.create( 'polling', {periodInMinutes: this.getPollIntervalTime()} );
-      this.polling( true );
+  chrome.runtime.onMessage.addListener( handler );
 
-    };
+}
 
-    chrome.runtime.onMessage.addListener( handler );
+function updateBadge( isSuccess ) {
 
-  }
+  const i = +isSuccess;
+  if ( isSuccess ) {
 
-  onPolling() {
-
-    chrome.alarms.create( 'polling', {periodInMinutes: this.getPollIntervalTime()} );
-    chrome.alarms.onAlarm.addListener( this.polling.bind( this ) );
-    this.polling( true );
-
-  }
-
-  onBadgeClicked() {
-
-    const handler = () => this.createTab().polling( true );
-    chrome.browserAction.onClicked.addListener( handler );
+    chrome.browserAction.setBadgeBackgroundColor( {color: [ 201, 51, 51, 1 ]} );
 
   }
 
-  updateBadge( isSuccess ) {
+  if ( typeof badgeData.text[ i ] !== 'undefined' ) {
 
-    const i = +isSuccess;
-    if ( isSuccess ) {
-
-      chrome.browserAction.setBadgeBackgroundColor( {color: [ 201, 51, 51, 1 ]} );
-
-    }
-
-    if ( typeof this.badgeData.text[ i ] !== 'undefined' ) {
-
-      chrome.browserAction.setIcon( {path: this.badgeData.path[ i ]} );
-      chrome.browserAction.setBadgeText( {text: this.badgeData.text[ i ]} );
-
-    }
-
-  }
-
-  createTab() {
-
-    const callback = ( tabs ) => {
-
-      for ( let i = 0, tab; tab = tabs[ i ]; i++ ) {
-
-        if ( tab.url && this.isMailUrl( tab.url ) ) {
-
-          // Update a tab.
-          chrome.tabs.update( tab.id, {selected: true} );
-          return;
-
-        }
-
-      }
-
-      chrome.tabs.create( {url: this.getMailBoxUrl()} );
-
-    };
-
-    chrome.tabs.getAllInWindow( null, callback );
-
-    return this;
-
-  }
-
-  polling( isAbort ) {
-
-    try {
-
-      if ( isAbort ) {
-
-        this.xhr.abort();
-
-      }
-
-      this.xhr.open( 'GET', this.getMailFeedUrl(), true );
-      this.xhr.onload = () => {
-
-        const isDone = this.xhr.readyState === 4;
-        const isSuccess = this.xhr.status === 200;
-        let isUpdate = false;
-
-        if ( this.isNoAccount( this.xhr.responseURL ) ) {
-
-          this.options.accountNumber = this.defaults.accountNumber;
-          chrome.storage.local.set( this.options );
-
-        }
-
-        if ( isDone && isSuccess ) {
-
-          const entries = this.xhr.responseXML.getElementsByTagName( 'entry' );
-          const entriesCount = String( entries.length === 0 ? '' : entries.length );
-          // Update of badge count.
-          isUpdate = this.badgeData.text[ 1 ] !== entriesCount;
-
-          this.badgeData.text[ 1 ] = entriesCount;
-          chrome.storage.local.set( this.badgeData );
-
-        }
-
-        if ( isUpdate ) this.badgeRotate();
-        this.updateBadge( isSuccess );
-
-      };
-
-      this.xhr.onerror = () => this.updateBadge( 0 );
-      this.xhr.send( null );
-
-    } catch ( error ) {
-
-      this.updateBadge( 0 );
-
-    }
-
-  }
-
-  getMailBaseUrl() {
-
-    return 'https://mail.google.com/mail/';
-
-  }
-
-  getMailBoxUrl() {
-
-    const number = this.options.accountNumber;
-    return `${this.getMailBaseUrl()}u/${number}/`;
-
-  }
-
-  getMailFeedUrl() {
-
-    return `${this.getMailBoxUrl()}feed/atom`;
-
-  }
-
-  getPollIntervalTime() {
-
-    return ( ( sec ) => {
-
-      return sec / 60;
-
-    } )( this.options.pollingInterval );
-
-  }
-
-  isMailUrl( url ) {
-
-    return ( url.replace( /^(.+)#.+/, '$1' ) ) === this.getMailBoxUrl();
-
-  }
-
-  isNoAccount( result ) {
-
-    return result !== this.getMailFeedUrl();
+    chrome.browserAction.setIcon( {path: badgeData.path[ i ]} );
+    chrome.browserAction.setBadgeText( {text: badgeData.text[ i ]} );
 
   }
 
 }
 
-const mailChecker = MailChecker.init( defaults );
+function createTab() {
 
+  const callback = ( tabs ) => {
+
+    for ( let i = 0, tab; tab = tabs[ i ]; i++ ) {
+
+      if ( tab.url && isMailUrl( tab.url ) ) {
+
+        // Update a tab.
+        chrome.tabs.update( tab.id, {selected: true} );
+        return;
+
+      }
+
+    }
+
+    chrome.tabs.create( {url: getMailBoxUrl()} );
+
+  };
+
+  chrome.tabs.getAllInWindow( null, callback );
+
+}
+
+function polling( isAbort ) {
+
+  try {
+
+    if ( isAbort ) {
+
+      xhr.abort();
+
+    }
+
+    xhr.open( 'GET', getMailFeedUrl(), true );
+    xhr.onload = () => {
+
+      const isDone = xhr.readyState === 4;
+      const isSuccess = xhr.status === 200;
+      let isUpdate = false;
+
+      if ( isNoAccount( xhr.responseURL ) ) {
+
+        options.accountNumber = defaults.accountNumber;
+        chrome.storage.local.set( options );
+
+      }
+
+      if ( isDone && isSuccess ) {
+
+        const entries = xhr.responseXML.getElementsByTagName( 'entry' );
+        const entriesCount = String( entries.length === 0 ? '' : entries.length );
+        // Update of badge count.
+        isUpdate = badgeData.text[ 1 ] !== entriesCount;
+
+        badgeData.text[ 1 ] = entriesCount;
+        chrome.storage.local.set( badgeData );
+
+      }
+
+      if ( isUpdate ) badgeAnimate.badgeRotate();
+      updateBadge( isSuccess );
+
+    };
+
+    xhr.onerror = () => updateBadge( 0 );
+    xhr.send( null );
+
+  } catch ( error ) {
+
+    updateBadge( 0 );
+
+  }
+
+}
+
+function getMailBaseUrl() {
+
+  return 'https://mail.google.com/mail/';
+
+}
+
+function getMailBoxUrl() {
+
+  const number = options.accountNumber;
+  return `${getMailBaseUrl()}u/${number}/`;
+
+}
+
+function getMailFeedUrl() {
+
+  return `${getMailBoxUrl()}feed/atom`;
+
+}
+
+function getPollIntervalTime() {
+
+  return ( ( sec ) => {
+
+    return sec / 60;
+
+  } )( options.pollingInterval );
+
+}
+
+function isMailUrl( url ) {
+
+  return ( url.replace( /^(.+)#.+/, '$1' ) ) === getMailBoxUrl();
+
+}
+
+function isNoAccount( result ) {
+
+  return result !== getMailFeedUrl();
+
+}
